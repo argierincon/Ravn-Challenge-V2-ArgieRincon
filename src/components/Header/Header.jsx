b26a479ab50b3b8f39a0514434140a2b3ce8ca55
argierdfj <argierincon@gmail.com>
2020-09-22 11:44:44 -0500

    Añadiendo componente header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.sass";
+
+const Header = () => {
+  return (
+    <header className="header h2 h2-default-color">
+      <h2>Ravn Star Wars Registry</h2>
+    </header>
+  );
+};
+
+export default Header;
