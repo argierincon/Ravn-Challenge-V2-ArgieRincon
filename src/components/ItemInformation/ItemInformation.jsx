3abf295acad1ad49234edcb02c815b9888948074
argierdfj <argierincon@gmail.com>
2020-09-22 17:44:52 -0500

    Añadiendo componente item information

---
+++ src/components/ItemInformation/ItemInformation.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ItemInformation.sass';
+
+const ItemInformation = () => {
+  return (
+    <div className="item-information">
+      <h2 className="h2 h2-low-emphasis-color">Eye Color</h2>
+      <h2 className="align-end h2 h2-default-color">Blue</h2>
+    </div>
+  );
+};
+
+export default ItemInformation;
