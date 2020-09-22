99e757aac5410665f88c864be9f3205b0c9f84bc
argierdfj <argierincon@gmail.com>
2020-09-22 17:46:33 -0500

    Añadiendo componente FailedLoad

---
+++ src/components/FailedLoadDrawer/FailedLoadDrawer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './FailedLoadDrawer.sass';
+
+const FailedLoadDrawer = () => {
+  return (
+    <div className="failed-load-drawer">
+      <h2 className="h2 h2-high-emphasis-color">Failed to Load Data</h2>
+    </div>
+  );
+};
+
+export default FailedLoadDrawer;
