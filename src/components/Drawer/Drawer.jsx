a87c6e306dc0a3fb79dee41e8484c1e6c88bf8fd
argierdfj <argierincon@gmail.com>
2020-09-23 19:17:17 -0500

    Añadiendo consulta al graphQL API

--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -1,31 +1,54 @@
+import { gql, useQuery } from '@apollo/client';
+import { Link } from 'react-router-dom';
 import React from 'react';
 import ItemDrawer from '../ItemDrawer/ItemDrawer';
 import LoadingDrawer from '../LoadingDrawer/LoadingDrawer';
 import FailedLoadDrawer from '../../components/FailedLoadDrawer/FailedLoadDrawer';
 import './Drawer.sass';
 
+export const queryAllPeopleSW = gql`
+  query queryAllPeopleSW {
+    allPeople {
+      people {
+        id
+        name
+        species {
+          name
+        }
+        homeworld {
+          name
+        }
+      }
+    }
+  }
+`;
+
 const Drawer = () => {
+  const { data, loading, error } = useQuery(queryAllPeopleSW, {
+    fetchPolicy: 'network-only',
+  });
+  // console.log(data);
+  // console.log(loading);
+  // console.log(error);
+
   return (
     <div className="drawer">
-      <ItemDrawer
-        name={'Luke Skywalker'}
-        species={'Human'}
-        homeworld={'Tatooine'}
-      />
-      <ItemDrawer name={'C-3PO'} species={'Droid'} homeworld={'Tatooine'} />
-      <ItemDrawer name={'R2-D2'} species={'Droid'} homeworld={'Naboo'} />
-      <ItemDrawer
-        name={'Darth Vader'}
-        species={'Human'}
-        homeworld={'Tatooine'}
-      />
-      <ItemDrawer
-        name={'Leia Organa'}
-        species={'Human'}
-        homeworld={'Alderaan'}
-      />
-      <LoadingDrawer />
-      <FailedLoadDrawer />
+      {error ? (
+        <FailedLoadDrawer />
+      ) : loading ? (
+        <LoadingDrawer />
+      ) : (
+        data.allPeople.people.map((ele) => (
+          <Link key={ele.id} to={`/${ele.id}`}>
+            <ItemDrawer
+              idPerson={ele.id}
+              name={ele.name}
+              species={ele.species == null ? 'Human' : ele.species.name}
+              homeworld={ele.homeworld.name}
+            />
+          </Link>
+        ))
+      )}
     </div>
   );
 };
--- src/components/GeneralCharacterInformation/GeneralCharacterInformation.jsx
+++ src/components/GeneralCharacterInformation/GeneralCharacterInformation.jsx
@@ -1,21 +1,59 @@
+import { gql, useQuery } from '@apollo/client';
+import { withRouter } from 'react-router-dom';
 import React from 'react';
 import Title from '../Title/Title';
 import ItemInformation from '../ItemInformation/ItemInformation';
+import Loading from '../Loading/Loading';
 import './GeneralCharacterInformation.sass';
 
-const GeneralCharacterInformation = () => {
+export const queryCharacterInfo = gql`
+  query queryCharacterInfo($idPerson: ID) {
+    person(id: $idPerson) {
+      eyeColor
+      hairColor
+      skinColor
+      birthYear
+      vehicleConnection {
+        vehicles {
+          name
+        }
+      }
+    }
+  }
+`;
+
+const GeneralCharacterInformation = (props) => {
+  let { id } = props.match.params;
+  id = id ? id : 'cGVvcGxlOjE=';
+  const { data, loading, error } = useQuery(queryCharacterInfo, {
+    fetchPolicy: 'network-only',
+    variables: { idPerson: id },
+  });
+  // console.log(data);
+  // console.log(loading);
+  // console.log(error);
+  if (loading) return <Loading />;
+
+  const {
+    eyeColor,
+    hairColor,
+    skinColor,
+    birthYear,
+    vehicleConnection,
+  } = data.person;
   return (
     <section className="general-character-information">
       <Title title={'General Information'} />
-      <ItemInformation itemInfo={'Eye Color'} itemDetail={'Blue'} />
-      <ItemInformation itemInfo={'Hair Color'} itemDetail={'Blond'} />
-      <ItemInformation itemInfo={'Skin Color'} itemDetail={'Fair'} />
-      <ItemInformation itemInfo={'Birth Year'} itemDetail={'19BBY'} />
-      <Title title={'Vehicles'} />
-      <ItemInformation itemInfo={'Snowspeeder'} />
-      <ItemInformation itemInfo={'Imperial Speeder Bike'} />
+      <ItemInformation itemInfo="Eye Color" itemDetail={eyeColor} />
+      <ItemInformation itemInfo="Hair Color" itemDetail={hairColor} />
+      <ItemInformation itemInfo="Skin Color" itemDetail={skinColor} />
+      <ItemInformation itemInfo="Birth Year" itemDetail={birthYear} />
+      {vehicleConnection.vehicles.length > 0 && <Title title={'Vehicles'} />}
+      {vehicleConnection.vehicles.map((ele) => (
+        <ItemInformation key={ele.name} itemInfo={ele.name} />
+      ))}
     </section>
   );
 };
 
-export default GeneralCharacterInformation;
+export default withRouter(GeneralCharacterInformation);
