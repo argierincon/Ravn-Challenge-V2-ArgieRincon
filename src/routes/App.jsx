af545aa9fe87d17f7250cdf4840f0b7709aed5a3
argierdfj <argierincon@gmail.com>
2020-09-23 19:19:52 -0500

    Añadiendo App

---
+++ src/routes/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from '../views/Home/Home.jsx';
+
+import '../assets/styles/Global.sass';
+
+const App = () => (
+  <BrowserRouter>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/:id" component={Home} />
+  </BrowserRouter>
+);
+
+export default App;
